62a5843dc8dd8096c85ac54a2b327bd41cd71d8f
Fedor <noi8e@ya.ru>
2020-02-26 10:46:15 +0300

    старт урока по функциям

---
+++ lesson 3/someFunc.js
@@ -0,0 +1,13 @@
+
+//декларативный способ объявления
+function test(num) {
+    console.log(num + 1);
+}
+
+test(1);
+// function expression
+let strFunc = function(str) {
+    console.log(`мое слово это ${str}`);
+}
+
+strFunc('kama');
