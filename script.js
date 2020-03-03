a843c7303f8831ead67dc2c2881cd2631bf1a1fa
Fedor <noi8e@ya.ru>
2020-03-03 21:44:00 +0300

    Перенес функции в объект

--- script.js
+++ script.js
@@ -22,73 +22,59 @@ let appData = {
     expenses: {},
     optionalExpenses: {},
     income: [],
-    savings: true
-};
-// функция для записи статей расходов и трат в appData.obj
-function chooseExpenses() {
-    for(let i = 0; i < 2; i++) {
-        // тут непонятно, как конкретезировать пару вопрос-ответ?
-        let requiredExpense = prompt("Введите обязательную статью расходов в этом месяце"),
-            spendValue = +prompt("Во сколько обойдется?");
-    
-         if ((typeof(requiredExpense)) === 'string' && (typeof(spendValue)) != null && requiredExpense != '' && spendValue != '' && requiredExpense.length < 50  ) {
-                appData.expenses[requiredExpense] = spendValue;
-         }
-         else {
-            i -= 1;
-         }
-    
-    };
-}
-chooseExpenses();
-//записываем необязательные расходы
-function chooseOptExpenses() {
-    for (i = 1; i <= 3; i++) {
-        let optionalExpenses = prompt(`статья необязательных расходов?`);
-        appData.optionalExpenses[i] = optionalExpenses;
-    }   
-};
-chooseOptExpenses();
-
-// считаем расход на день. 
-function detectLevel() {
-    appData.moneyPerDay = (appData.budget / 30).toFixed();
-    if(appData.moneyPerDay) {
-        alert(`ваш расход за день в месяц где-то ${appData.moneyPerDay}`);
-    }
-};
-detectLevel();
- 
-
-function detectDayBudget() {
-    if (appData.moneyPerDay < 700) {
-        alert("вы умеете тратить деньги");
-    }
-    if (appData.moneyPerDay > 700 && appData.moneyPerDay < 2000) {
-        console.log("нормально тратите, возможно надо посчитать ваши доходы");
-    }
-    if(appData.moneyPerDay > 2000 && appData.moneyPerDay >=4000) {
-        alert('гуляй-шикуй!');
-    }
-    else {
-        console.log("что-то пошло не так");
-    }
-    
-    console.log(appData);
-
-};
-detectDayBudget();
-
-
-function checkSavings() {
-    if (appData.savings == true) {
-        let save = +prompt('сколько накопил?'),
-            percent = +prompt('под какой процент?');
-            
-        appData.monthIncome = save/100/12*percent;
-        alert(`доход в месяц с вашего депозита ${(appData.monthIncome).toFixed()}`);
+    savings: true,
+    chooseExpenses: function() {
+        for(let i = 0; i < 2; i++) {
+            // тут непонятно, как конкретезировать пару вопрос-ответ?
+            let requiredExpense = prompt("Введите обязательную статью расходов в этом месяце"),
+                spendValue = +prompt("Во сколько обойдется?");
         
+             if ((typeof(requiredExpense)) === 'string' && (typeof(spendValue)) != null && requiredExpense != '' && spendValue != '' && requiredExpense.length < 50  ) {
+                    appData.expenses[requiredExpense] = spendValue;
+             }
+             else {
+                i -= 1;
+             }
+        
+        };
+    },
+    chooseOptExpenses: function() {
+        for (i = 1; i <= 3; i++) {
+            let optionalExpenses = prompt(`статья необязательных расходов?`);
+            appData.optionalExpenses[i] = optionalExpenses;
+        }   
+    },
+    detectLevel: function() {
+        appData.moneyPerDay = (appData.budget / 30).toFixed();
+        if(appData.moneyPerDay) {
+            alert(`ваш расход за день в месяц где-то ${appData.moneyPerDay}`);
+        }
+    },
+    detectDayBudget: function() {
+        if (appData.moneyPerDay < 700) {
+            alert("вы умеете тратить деньги");
+        }
+        if (appData.moneyPerDay > 700 && appData.moneyPerDay < 2000) {
+            console.log("нормально тратите, возможно надо посчитать ваши доходы");
+        }
+        if(appData.moneyPerDay > 2000 && appData.moneyPerDay >=4000) {
+            alert('гуляй-шикуй!');
+        }
+        else {
+            console.log("что-то пошло не так");
+        }  
+        console.log(appData);
+    },
+    checkSavings: function() {
+        if (appData.savings == true) {
+            let save = +prompt('сколько накопил?'),
+                percent = +prompt('под какой процент?');
+                
+            appData.monthIncome = save/100/12*percent;
+            alert(`доход в месяц с вашего депозита ${(appData.monthIncome).toFixed()}`);
+            
+        }
     }
-}
-checkSavings();
-//
+
+
+};
