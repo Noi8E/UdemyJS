e3f064f643c32299c34ae921625cfedacf06dfea
Fedor <noi8e@ya.ru>
2020-03-25 23:46:09 +0300

    добавил elem в переменные

--- budget_app/js/budget.script.js
+++ budget_app/js/budget.script.js
@@ -1,3 +1,10 @@
+//N.B.  Если не можешь достучаться до единственного эл-та через .getElementsByClassName, то нужно через
+// индекс .getElementsByClassName[0] без индекса код не поймет что это объект...надо уточнить
+
+
+
+
+
 let startButton = document.getElementById('start'),
     budgetValue = document.getElementsByClassName('budget-value')[0],
     daybudgetValue = document.getElementsByClassName('daybudget-value')[0],
@@ -6,10 +13,29 @@ let startButton = document.getElementById('start'),
     optionalExpensesValue = document.getElementsByClassName('optionalexpenses-value')[0],
     incomeValue = document.getElementsByClassName('income-value')[0],
     monthSavingsValue = document.getElementsByClassName('monthsavings-value')[0],
-    yearsavingsValue = document.getElementsByClassName('yearsavings-value')[0];
+    yearsavingsValue = document.getElementsByClassName('yearsavings-value')[0],
+    //get Inputs 
+    expensesItems = document.getElementsByClassName('expenses-item'),
+    //Утвердить/рассчитать
+    expensesItemBtn = document.getElementsByTagName('button')[0],
+    optionalexpensesBtn = document.getElementsByTagName('button')[1],
+    countBudgetBtn = document.getElementsByTagName('button')[2],
+    //get optional Expenses Inputs
+    optionalexpensesItem = document.querySelectorAll('.optionalexpenses-item'),
+    // other items
+    chooseIncome = document.querySelector('.choose-income'),
+    checkboxSavings = document.querySelector('.checksavings input'),
+    chooseSum = document.querySelector('.choose-sum'), 
+    choosePercent = document.querySelector('.choose-percent'),
+    yearValue = document.querySelector('.year-value'),
+    monthValue = document.querySelector('.month-value'),
+    dayValue = document.querySelector('.day-value');
 
 
 
+
+
+    // (статьи возможного дохода, чекбокс, сумма, процент, год, месяц, день)
 
 
 
@@ -20,30 +46,3 @@ console.log(budgetValue);
 
 
 
-//N.B.  Если не можешь достучаться до единственного эл-та через .getElementsByClassName, то нужно через
-// индекс .getElementsByClassName[0] без индекса код не поймет что это объект...надо уточнить
-
-
-/* <div class="budget">Доход: </div>
-<div class="budget-value"></div>
-
-<div class="daybudget">Бюджет на 1 день: </div>
-<div class="daybudget-value"></div>
-
-<div class="level">Уровень дохода: </div>
-<div class="level-value"></div>
-
-<div class="expenses">Обязательные расходы: </div>
-<div class="expenses-value"></div>
-
-<div class="optionalexpenses">Возможные траты: </div>
-<div class="optionalexpenses-value"></div>
-
-<div class="income">Дополнительный доход: </div>
-<div class="income-value"></div>
-
-<div class="monthsavings">Накопления за 1 месяц:</div>
-<div class="monthsavings-value"></div>
-
-<div class="yearsavings">Накопления за 1 год:</div>
-<div class="yearsavings-value"></div> */
