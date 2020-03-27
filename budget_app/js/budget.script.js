f92098b71f150a0f5db56d54673ee4a0102ee273
Fedor <noi8e@ya.ru>
2020-03-27 14:02:58 +0300

    дошел до шага с выводом доп расзодов на экран

--- budget_app/js/budget.script.js
+++ budget_app/js/budget.script.js
@@ -31,18 +31,120 @@ let startButton = document.getElementById('start'),
     monthValue = document.querySelector('.month-value'),
     dayValue = document.querySelector('.day-value');
 
+    let money, time;
+    //корячим обработчик + функцию с обработкой введенных величин
+    startButton.addEventListener('click', function() {
+        time = prompt("Введите дату в формате YYYY-MM-DD"),
+        money = +prompt("Ваш бюджет на месяц?");
+    
+        while (isNaN(money) || money === '' || money === null) {
+            money = +prompt("Ваш бюджет на месяц2?");
+        }
+        appData.budget = money;
+        appData.timeData = time;
+        budgetValue.textContent = money.toFixed();
+        //если у нас инпут, то работаем с .value
+        yearValue.value = new Date(Date.parse(time)).getFullYear();
+        monthValue.value = new Date(Date.parse(time)).getMonth() + 1;
+        dayValue.value = new Date(Date.parse(time)).getDate();
+    })
+    //готовим константу, в которую положим кол-во инпутов для обязателных расходов
+    //выносим ее в область глобальной видимости ибо мжем себе позволить
+    //она считает именно общее кол-во инпутов, т.к. нам нужно знать общее значение аллертов
+    //Сегментирование по конкретным типам инпутов будет происходит именно в цикле
+    const expensesInputsValue = expensesItems.length;
+    //корячим обработчик + функцию с суммой обязательных расходов.
+    expensesItemBtn.addEventListener('click', function(){
+    //переменная, в которую суммируем все расходы
+        let sum = 0; 
+        for(let i = 0; i < expensesInputsValue ; i++) {
+            let a = expensesItems[i].value;
+            let b = expensesItems[++i].value;
+                if ((typeof(a)) === 'string' && (typeof(b)) != null && a != '' && b != '' && a.length < 50  ) {
+            appData.expenses[a] = b;
+            sum += +b;
+            }
+            else {
+                i -= 1;
+                alert('Вы ввели невалдиные данные');
+            }
+        }
+        expensesValue.textContent = sum + ' рублей';
+    });
 
 
-
-
-    // (статьи возможного дохода, чекбокс, сумма, процент, год, месяц, день)
-
-
-
-
-
-console.log(startButton);
-console.log(budgetValue);
-
+    //обработчик с доп.расходами
+    optionalexpensesBtn.addEventListener('click', function(){
+        for (let i = 0; i < optionalexpensesItem.length; i++) {
+            let opt = optionalexpensesItem[i].value;
+            appData.optionalExpenses[i] = opt;
+            optionalExpensesValue.textConten += appData.optionalExpenses[i] + ' ';
+        }   
+    })
+    
+    // объект-хранилище
+    let appData = {
+        budget: money,
+        timeData: time,
+        expenses: {},
+        optionalExpenses: {},
+        income: [],
+        savings: true,
+        detectLevel: function() {
+            appData.moneyPerDay = (appData.budget / 30).toFixed();
+            if(appData.moneyPerDay) {
+                alert(`ваш расход за день в месяц где-то ${appData.moneyPerDay}`);
+            }
+        },
+        detectDayBudget: function() {
+            if (appData.moneyPerDay < 700) {
+                alert("вы умеете тратить деньги");
+            }
+            if (appData.moneyPerDay > 700 && appData.moneyPerDay < 2000) {
+                console.log("нормально тратите, возможно надо посчитать ваши доходы");
+            }
+            if(appData.moneyPerDay > 2000 && appData.moneyPerDay >=4000) {
+                alert('гуляй-шикуй!');
+            }
+            else {
+                console.log("что-то пошло не так");
+            }  
+            console.log(appData.moneyPerDay);
+        },
+        checkSavings: function() {
+            if (appData.savings == true) {
+                let save = +prompt('сколько накопил?'),
+                    percent = +prompt('под какой процент?');
+                    
+                appData.monthIncome = save/100/12*percent;
+                alert(`доход в месяц с вашего депозита ${(appData.monthIncome).toFixed()}`);
+                
+            }
+        },
+        chooseIncome: function() {
+            items = prompt('введите доп. доходы через зпт.', "");
+            while (typeof items != 'string' || !items || items == null) {
+                items = prompt('введите доп. доходы через зпт.', "");   
+            }
+            appData.income = items.split(', '); 
+            //Если юзер вводит несколько значений, как их подпушить в массив?
+            //Созздаем временнный массив, а потом схлопываем через .push.apply
+            appData.income.push(prompt('может что-то запамятовали?', ""));
+            appData.income.sort();
+    
+        },
+        bonusesIncome: function() {
+            appData.income.forEach((item, i) => {
+                alert(`способы дополнительного заработка ${i+1} : ${item}`);
+            }); 
+        },
+        showMeAll: function() {
+            for (let key in appData) {
+                console.log(`Наша программа включает в себя данные: ${appData[key]}`);
+            };
+        }
+    
+    
+    };
 
 
