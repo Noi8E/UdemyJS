ac30844291fd5e2db2727b0a311c0659c9d02071
Fedor <noi8e@ya.ru>
2020-04-03 14:51:15 +0300

    начал разбор с модулями

---
+++ JEST-SAMPLE/parentModule.js
@@ -0,0 +1,8 @@
+// module.exports function sum(a, b) {
+//     return a + b;
+//   }
+
+module.exports let months = ['Jan', 'Feb', 'Mar','Apr', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
+
+
+console.log(months);
---
+++ JEST-SAMPLE/test.js
@@ -0,0 +1,8 @@
+import { sum, months } from './parentModule';
+import { forExportExample } from '../exportExample';
+
+
+
+
+console.log(forExportExample);
+console.log(months);
